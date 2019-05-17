e916de6fac695924b3190fa818d1172a01d89211
Ephraim Innocent <info.ephraims@gmail.com>
2019-05-17 13:29:25 +0100

    Initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Support Engine`,
+    description: `Customer Support for tech businesses`,
+    author: `Roosevelt (TheCodeist)`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
