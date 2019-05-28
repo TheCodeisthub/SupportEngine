81c076ffba3e79be63fa99ad7e3f86106db98eef
Ephraim Innocent <info.ephraims@gmail.com>
2019-05-28 11:12:35 +0100

    Initail commit

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,6 +3,7 @@ import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import pnf from "../images/page_not_found.svg"
+import { Link } from "gatsby"
 
 const NotFoundPage = () => (
   <Layout>
@@ -11,6 +12,12 @@ const NotFoundPage = () => (
       <div className="container">
         <img src={pnf} width="100vw" alt="404" />
         <p>You just hit a route that doesn&#39;t exist...</p>
+        <small>
+          don&#39;t find what you are looking for?{" "}
+          <strong>
+            <Link to="/contact">Contact us</Link>
+          </strong>
+        </small>
       </div>
     </div>
   </Layout>
