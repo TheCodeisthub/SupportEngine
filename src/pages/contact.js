d16b82b4646f08ee477a56d9eaf2185156ee3ca0
Ephraim Innocent <info.ephraims@gmail.com>
2019-05-28 11:46:49 +0100

    Initial commit

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -27,32 +27,12 @@ const ContactPage = () => (
       <p style={{ textAlign: "left", marginBottom: "10vh" }}>
         Alternatetivly, you can use the contact form below.
       </p>
-      <form name="contact" method="POST" data-netlify="true">
-        <p>
-          <label>
-            Your Name: <input type="text" name="name" />
-          </label>
-        </p>
-        <p>
-          <label>
-            Your Email: <input type="email" name="email" />
-          </label>
-        </p>
 
-        <p>
-          <label>
-            Message: <textarea name="message" />
-          </label>
-        </p>
-        <p>
-          <button type="submit">Send</button>
-        </p>
-      </form>
-
-      {/* <form
+      <form
         name="contact"
         method="POST"
         data-netlify="true"
+        data-netlify-honeypot="bot-field"
         className="contact_form"
       >
         <div className="input-c">
@@ -99,7 +79,29 @@ const ContactPage = () => (
             Submit
           </button>
         </span>
-      </form> */}
+      </form>
+
+      {/* <form name="contact" method="POST" data-netlify="true">
+      <p>
+        <label>
+          Your Name: <input type="text" name="name" />
+        </label>
+      </p>
+      <p>
+        <label>
+          Your Email: <input type="email" name="email" />
+        </label>
+      </p>
+    
+      <p>
+        <label>
+          Message: <textarea name="message" />
+        </label>
+      </p>
+      <p>
+        <button type="submit">Send</button>
+      </p>
+    </form> */}
     </section>
   </Layout>
 )
